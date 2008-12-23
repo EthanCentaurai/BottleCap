d57af0ee5a5581340dd7812c4d18b876f2a3fa76
Ethan Centaurai <ethan.centaurai@live.co.uk>
2008-12-23 15:15:29 +0000

    Now works without Prat-3.0! :D

--- Core.lua
+++ Core.lua
@@ -1,59 +1,64 @@
---[[
-Name: Prat BottleCap
-Author: Ethan Centaurai
-Description: Converts ALL CAPS CHAT into all lower case chat for the end user
-Dependencies: Prat-3.0
-]]--
 
-local PRAT_MODULE = Prat:RequestModuleName("BottleCap")
-if PRAT_MODULE == nil then return end
+local BottleCap = LibStub("AceAddon-3.0"):NewAddon("BottleCap")
+local db
 
-local BottleCap = Prat:NewModule(PRAT_MODULE)
+local function bottleCaps(msg)
+	if db.verbose or msg == msg:upper() then
+		-- Verbose Mode = all chat becomes lower case, regardless of original case
+		-- else all ALL CAPS CHAT becomes lower case
+		msg = msg:lower()
 
+		-- Fix links
+		msg = msg:gsub("|hitem", "|Hitem")
+		msg = msg:gsub("|hquest", "|Hquest")
+		msg = msg:gsub("|hspell", "|Hspell")
+		msg = msg:gsub("|htalent", "|Htalent")
+		msg = msg:gsub("|hachievement", "|Hachievement")
+		msg = msg:gsub("|htrade", "|Htrade")
+		msg = msg:gsub("|henchant", "|Henchant")
 
-Prat:SetModuleDefaults(BottleCap.name, { profile = { on = true, verbose = false }})
-Prat:SetModuleOptions(BottleCap, {
-	name = "BottleCap",
-	desc = "Converts ALL CAPS CHAT into all lower case chat for the end user",
-	type = "group",
-	args = {
-		verbose = {
-			name = "Verbose Mode",
-			desc = "Convert all chat, regardless of original case, into lowercase text.",
-			type = "toggle", order = 1,
-		},
-	},
-})
-
-
-function BottleCap:OnModuleEnable()
-	Prat.RegisterChatEvent(self, "Prat_PreAddMessage")
+		return false, msg
+	end
 end
 
-function BottleCap:OnModuleDisable()
-	Prat.UnregisterAllChatEvents(self)
+
+function BottleCap:OnEnable()
+	self.db = LibStub("AceDB-3.0"):New("BottleCapDB", { profile = { verbose = false }}, "Default")
+
+	db = self.db.profile
+
+	LibStub("AceConfig-3.0"):RegisterOptionsTable("BottleCap", {
+		name = "Bottle Cap",
+		desc = "Converts ALL CAPS CHAT into all lowercase chat.",
+		type = "group",
+		get = function(key) return db[key.arg] end,
+		set = function(key, value) db[key.arg] = value end,
+		args = {
+			verbose = {
+				name = "Verbose Mode",
+				desc = "Convert all chat into lowercase text, regardless of original case.",
+				type = "toggle", order = 1, arg = "verbose",
+			},
+		}, 
+	})
+
+	LibStub("AceConfigDialog-3.0"):AddToBlizOptions("BottleCap", "Bottle Cap")
+
+	-- I hate ugleh hax :(
+	if IsAddOnLoaded("Prat-3.0") then
+		Prat.RegisterChatEvent(self, "Prat_PreAddMessage")
+		print("Prat raped successfully")
+	else
+		ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", bottleCaps)
+		ChatFrame_AddMessageEventFilter("CHAT_MSG_SAY", bottleCaps)
+		ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", bottleCaps)
+		ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", bottleCaps)
+	end
 end
 
 function BottleCap:Prat_PreAddMessage(_, message, frame, event, t, r, g, b)
 	if event == "CHAT_MSG_SYSTEM" then return end
 
-	local msg = message.MESSAGE
-
-	if self.db.profile.verbose or msg == msg:upper() then
-		-- Verbose Mode = all chat becomes lower case, regardless of original case
-		-- else all ALL CAPS CHAT becomes lower case
-		msg = msg:lower()
-	end
-
-	-- Fix links
-	msg = msg:gsub("|hitem", "|Hitem")
-	msg = msg:gsub("|hquest", "|Hquest")
-	msg = msg:gsub("|hspell", "|Hspell")
-	msg = msg:gsub("|htalent", "|Htalent")
-	msg = msg:gsub("|hachievement", "|Hachievement")
-	msg = msg:gsub("|htrade", "|Htrade")
-	msg = msg:gsub("|henchant", "|Henchant")
-
-	message.MESSAGE = msg
+	_, message.MESSAGE = bottleCaps(message.MESSAGE)
 end
 
